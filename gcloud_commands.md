f602de447e646432ab71d603a25b3ae4dd86ca9c
talantasan <talant.py@gmail.com>
2025-09-23 13:52:31 -0500

    networking

--- gcloud_commands.md
+++ gcloud_commands.md
@@ -19,12 +19,13 @@ gcloud config set proejct $PROJECT_ID
 ```
 ## 4. gcloud compute instances
 ```
+gcloud compute regions list
 gcloud compute instances list
  gcloud compute instances delete talant-vm-1 --zone us-east1-b
 
  gcloud compute instance-templates list
 ```
-## 5. gcloud storage
+## 5. gcloud storage AND gsutil
 ```
  gcloud storage buckets create gs://talant-1-471921 --project talant-1-471921
  
@@ -33,5 +34,25 @@ gcloud compute instances list
  gcloud storage buckets list
 
  gcloud storage buckets list --format "json(name)"
+
+ gcloud storate cp [FILE_NAME] gs://[BUCKET_NAME]
+
+ gsutil mb gs://[BUCKET_NAME]
+
+ gsutil cp [FILE_NAME] gs://[BUCKET_NAME]
+```
+
+## 6. Enable google apis
+```
+gcloud services enable \
+iap.googleapis.com \
+networkmanagement.googleapis.com
+``` 
+
+## 7. SSH into VM machine
+```
+gcloud compute ssh mynet-us-vm \
+--zone=us-east1-c \
+--tunnel-through-iap 
 ```
 
--- terraform/instances/main.tf
+++ terraform/instances/main.tf
@@ -1,42 +1,43 @@
-resource "google_compute_instance" "vm_instance" {
-    name         = "talant-vm1"
+resource "google_compute_instance" "apache_instance" {
+    name         = "talant-apache-web-server"
     machine_type = "e2-micro"
-    zone         = "us-central1-a"
+    zone         = "us-central1-a" # Replace with your desired zone
 
     boot_disk {
-        initialize_params {
-            image = "debian-cloud/debian-12"
+    initialize_params {
+        image = "debian-cloud/debian-11" # Or your preferred image
         }
     }
 
     network_interface {
-        network = "default"
-        access_config {}
+    network = "default" # Or your custom VPC network
+    access_config {
+        // Ephemeral IP
+        }
     }
 
-    metadata_startup_script = <<-EOT
-        #!/bin/bash
-        sudo su
-        echo "Hello, World!" > /var/log/startup-script.log
-        apt-get update
-        apt-get install -y apache2
-        echo "Hello, World from $(hostname) $(hostname -i) > /var/www/html/index.html"
-        systemctl start apache2
-    EOT
+    metadata_startup_script = <<-EOF
+    #!/bin/bash
+    sudo apt-get update -y
+    sudo apt-get install apache2 -y
+    sudo systemctl enable apache2
+    sudo systemctl start apache2
+    echo "<h1>Hello from Talant Apache2 web server $(hostname) $(hostname -i)!</h1>" | sudo tee /var/www/html/index.html
+    sudo systemctl restart apache2
+    EOF
 
-    tags = ["http-server"]
+    tags = ["http-server"] # Used for firewall rules
 }
 
 resource "google_compute_firewall" "allow_http" {
-    name    = "allow-http"
-    network = "default"
+    name    = "talant-allow-http-80"
+    network = "default" # Or your custom VPC network
 
     allow {
-        protocol = "tcp"
-        ports    = ["80"]
+    protocol = "tcp"
+    ports    = ["80"]
     }
 
-    source_ranges = ["0.0.0.0/0"]
-    target_tags   = ["http-server"]
+    source_ranges = ["0.0.0.0/0"] # Allow traffic from all IP addresses
+    target_tags   = ["http-server"] # Apply this rule to instances with the "http-server" tag
 }
-
